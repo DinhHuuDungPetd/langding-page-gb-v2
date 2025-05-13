f564fb817c53bd8be6dc7f3e846ba3d430221dae
HoangTran <HoangTranHoangkhongmacao@gmail.com>
2025-05-13 09:16:24 +0700

    fix respo

--- src/component/pages/order/OrderPage.js
+++ src/component/pages/order/OrderPage.js
@@ -35,25 +35,30 @@ export default function ServicePage() {
     return (
         <div>
             <div className="relative w-full h-[200px] bg-[rgba(57,139,64,0.81)] bg-no-repeat bg-cover bg-[url(https://res.cloudinary.com/dgfwxibj4/image/upload/v1747017609/backgroundMobile/qykvzxbefmni2ertajlb.jpg)] bg-cover md:bg-[url(https://res.cloudinary.com/ddnasugap/image/upload/q_auto,f_auto/v1745825602/greenlab/z035deuonafdinctewby.webp)] bg-blend-multiply">
-                <div className="absolute bottom-4 left-5 md:left-10 lg:left-20  text-white text-2xl medium-italic z-9">
+                <div className="absolute bottom-4 left-5 md:left-10 lg:left-20  text-white text-md medium-italic z-9">
                     Trang chủ &gt; <span className="font-normal">Đặt lịch xét nghiệm</span>
                 </div>
             </div>
             <div className="container mx-auto py-8 ">
-                <h2 className='text-center text-5xl uppercase text-primary font-bold my-4'>Quy trình lấy mẫu tại green lab</h2>
+                <h2 className='text-center text-2xl uppercase text-primary font-bold my-4'>Quy trình lấy mẫu tại green lab</h2>
                 <div className=" w-full  flex justify-center">
-                    <div className="w-fit pt-8 flex justify-center relative px-4">
-                        <div className="absolute inset-x-0 top-1/3 h-[1px] bg-[#b3b3b3] z-0">
-                        </div>
-                        {items.map((item, index) => (
-                            <CircleComponent key={index} item={item} index={index} />
+                    <div className="w-full pt-8 flex justify-center relative px-4">
+                        <div
+                            className="absolute bg-[#b3b3b3] z-0 hidden md:block md:w-full md:h-[1px] md:left-0 md:top-[27%] lg:top-[33%] xl:top-[36%]"
+                        />
 
-                        ))}
+
+                        <div className="grid grid-cols-1 md:grid-cols-4 place-items-center md:place-items-start gap-8">
+                            {items.map((item, index) => (
+                                <CircleComponent key={index} item={item} index={index} />
+                            ))}
+                        </div>
                     </div>
+
                 </div>
             </div>
 
             <OrderComponent />
-        </div>
+        </div >
     )
 }
--- src/component/pages/service/component/MedicalSlider.js
+++ src/component/pages/service/component/MedicalSlider.js
@@ -3,7 +3,6 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
-import Image from "next/image"
 import "swiper/css";
 import "swiper/css/navigation";
 import Service_01 from "@/icons/Service_01"
@@ -17,7 +16,7 @@ import Service_08 from "@/icons/Service_08"
 
 const MedicalSwiper = () => {
     return (
-        <div className="relative container md:mx-auto py-5 px-10">
+        <div className="relative container md:mx-auto py-5">
             <Swiper
                 modules={[Navigation]}
                 spaceBetween={5}
@@ -39,110 +38,112 @@ const MedicalSwiper = () => {
                 className="w-full"
             >
                 <SwiperSlide>
-                    <div className="relative bg-white border-3 border-central rounded-2xl pt-5 pb-5 text-center shadow-sm m-10 w-[90%]">
+                    <div className="relative bg-white border-3 border-central rounded-2xl pt-5 md:pt-12 pb-5 text-center shadow-sm mx-2 my-8 md:m-10 w-[85%] md:w-[80%]">
                         <div className="absolute -top-5 left-1/2 transform -translate-x-1/2">
                             <div className="w-20 h-20 rounded-full bg-gradient-to-b from-primary to-midnight border-4 border-central flex items-center justify-center shadow-md">
                                 <Service_01 />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <h3 className="font-fz-poppins font-bold text-lg uppercase text-primary  mb-2 text-pretty">XÉT NGHIỆM<br /> HÓA SINH</h3>
-                            <p className="font-fz-poppins font-midium text-md text-primary">Xem thêm <span>»</span></p>
+                            <h3 className="font-fz-poppins font-bold text-md lg:text-xl uppercase text-primary  mb-2 text-pretty">XÉT NGHIỆM<br /> HÓA SINH</h3>
+                            <p className="font-fz-poppins font-midium text-sm lg:text-md text-primary">Xem thêm <span>»</span></p>
                         </div>
                     </div>
-                    <div className="relative bg-white border-3 border-central bg-gradient-to-b from-primary to-midnight rounded-2xl pt-5 pb-5 text-center shadow-sm m-10 w-[90%]">
+                    <div className="relative bg-white border-3 border-central bg-gradient-to-b from-primary to-midnight rounded-2xl pt-5 md:pt-12 pb-5 text-center shadow-sm mx-2 my-8 md:m-10 w-[85%] md:w-[80%]">
                         <div className="absolute -top-5 left-1/2 transform -translate-x-1/2">
                             <div className="w-20 h-20 rounded-full bg-white border-4 border-midnight flex items-center justify-center shadow-md">
                                 <Service_02 />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <h3 className="font-fz-poppins font-bold text-lg uppercase text-white  mb-2 text-pretty">XÉT NGHIỆM<br /> VI SINH</h3>
-                            <p className="font-fz-poppins font-midium text-md text-white">Xem thêm <span>»</span></p>
+                            <h3 className="font-fz-poppins font-bold text-md lg:text-xl uppercase text-white  mb-2 text-pretty">XÉT NGHIỆM<br /> VI SINH</h3>
+                            <p className="font-fz-poppins font-midium text-sm lg:text-md text-white">Xem thêm <span>»</span></p>
                         </div>
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className="relative bg-white border-3 border-central bg-gradient-to-b from-primary to-midnight rounded-2xl pt-5 pb-5 text-center shadow-sm m-10 w-[90%]">
+                    <div className="relative bg-white border-3 border-central bg-gradient-to-b from-primary to-midnight rounded-2xl pt-5 md:pt-12 pb-5 text-center shadow-sm mx-2 my-8 md:m-10 w-[85%] md:w-[80%]">
                         <div className="absolute -top-5 left-1/2 transform -translate-x-1/2">
                             <div className="w-20 h-20 rounded-full bg-white border-4 border-midnight flex items-center justify-center shadow-md">
                                 <Service_04 />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <h3 className="font-fz-poppins font-bold text-lg uppercase text-white  mb-2 text-pretty">XÉT NGHIỆM <br /> HUYẾT HỌC</h3>
-                            <p className="font-fz-poppins font-midium text-md text-white">Xem thêm <span>»</span></p>
+                            <h3 className="font-fz-poppins font-bold text-md lg:text-xl uppercase text-white  mb-2 text-pretty">XÉT NGHIỆM <br /> HUYẾT HỌC</h3>
+                            <p className="font-fz-poppins font-midium text-sm lg:text-md text-white">Xem thêm <span>»</span></p>
                         </div>
                     </div>
-                    <div className="relative bg-white border-3 border-central rounded-2xl pt-5 pb-5 text-center shadow-sm m-10 w-[90%]">
+                    <div className="relative bg-white border-3 border-central rounded-2xl pt-5 md:pt-12 pb-5 text-center shadow-sm mx-2 my-8 md:m-10 w-[85%] md:w-[80%]">
                         <div className="absolute -top-5 left-1/2 transform -translate-x-1/2">
                             <div className="w-20 h-20 rounded-full bg-gradient-to-b from-primary to-midnight border-4 border-central flex items-center justify-center shadow-md">
                                 <Service_03 />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <h3 className="font-fz-poppins font-bold text-lg uppercase text-primary  mb-2 text-pretty">XÉT NGHIỆM <br /> SINH HỌC PHÂN TỬ</h3>
-                            <p className="font-fz-poppins font-midium text-md text-primary">Xem thêm <span>»</span></p>
+                            <h3 className="font-fz-poppins font-bold text-md lg:text-xl uppercase text-primary  mb-2 text-pretty">XÉT NGHIỆM <br className="hidden md:block" /> SINH HỌC PHÂN TỬ</h3>
+                            <p className="font-fz-poppins font-midium text-sm lg:text-md text-primary">Xem thêm <span>»</span></p>
                         </div>
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className="relative bg-white border-3 border-central rounded-2xl pt-5 pb-5 text-center shadow-sm m-10 w-[90%]">
+                    <div className="relative bg-white border-3 border-central rounded-2xl pt-5 md:pt-12 pb-5 text-center shadow-sm mx-2 my-8 md:m-10 w-[85%] md:w-[80%]">
                         <div className="absolute -top-5 left-1/2 transform -translate-x-1/2">
                             <div className="w-20 h-20 rounded-full bg-gradient-to-b from-primary to-midnight border-4 border-central flex items-center justify-center shadow-md">
                                 <Service_07 />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <h3 className="font-fz-poppins font-bold text-lg uppercase text-primary  mb-2 text-pretty">XÉT NGHIỆM<br /> MIỄN DỊCH</h3>
-                            <p className="font-fz-poppins font-midium text-md text-primary">Xem thêm <span>»</span></p>
+                            <h3 className="font-fz-poppins font-bold text-md lg:text-xl uppercase text-primary  mb-2 text-pretty">XÉT NGHIỆM<br /> MIỄN DỊCH</h3>
+                            <p className="font-fz-poppins font-midium text-sm lg:text-md text-primary">Xem thêm <span>»</span></p>
                         </div>
                     </div>
-                    <div className="relative bg-white border-3 border-central bg-gradient-to-b from-primary to-midnight rounded-2xl pt-5 pb-5 text-center shadow-sm m-10 w-[90%]">
+                    <div className="relative bg-white border-3 border-central bg-gradient-to-b from-primary to-midnight rounded-2xl pt-5 md:pt-12 pb-5 text-center shadow-sm mx-2 my-8 md:m-10 w-[85%] md:w-[80%]">
                         <div className="absolute -top-5 left-1/2 transform -translate-x-1/2">
                             <div className="w-20 h-20 rounded-full bg-white border-4 border-midnight flex items-center justify-center shadow-md">
                                 <Service_06 />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <h3 className="font-fz-poppins font-bold text-lg uppercase text-white  mb-2 text-pretty">XÉT NGHIỆM<br /> GIẢI PHẪU BỆNH</h3>
-                            <p className="font-fz-poppins font-midium text-md text-white">Xem thêm <span>»</span></p>
+                            <h3 className="font-fz-poppins font-bold text-md lg:text-xl uppercase text-white  mb-2 text-pretty">XÉT NGHIỆM<br /> GIẢI PHẪU BỆNH</h3>
+                            <p className="font-fz-poppins font-midium text-sm lg:text-md text-white">Xem thêm <span>»</span></p>
                         </div>
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className="relative bg-white border-3 border-central bg-gradient-to-b from-primary to-midnight rounded-2xl pt-5 pb-5 text-center shadow-sm m-10 w-[90%]">
+                    <div className="relative bg-white border-3 border-central bg-gradient-to-b from-primary to-midnight rounded-2xl pt-5 md:pt-12 pb-5 text-center shadow-sm mx-2 my-8 md:m-10 w-[85%] md:w-[80%]">
                         <div className="absolute -top-5 left-1/2 transform -translate-x-1/2">
                             <div className="w-20 h-20 rounded-full bg-white border-4 border-midnight flex items-center justify-center shadow-md">
                                 <Service_08 />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <h3 className="font-fz-poppins font-bold text-lg uppercase text-white  mb-2 text-pretty">XÉT NGHIỆM<br /> TẠI NHÀ</h3>
-                            <p className="font-fz-poppins font-midium text-md text-white">Xem thêm <span>»</span></p>
+                            <h3 className="font-fz-poppins font-bold text-md lg:text-xl uppercase text-white  mb-2 text-pretty">XÉT NGHIỆM<br /> TẠI NHÀ</h3>
+                            <p className="font-fz-poppins font-midium text-sm lg:text-md text-white">Xem thêm <span>»</span></p>
                         </div>
                     </div>
-                    <div className="relative bg-white border-3 border-central rounded-2xl pt-5 pb-5 text-center shadow-sm m-10 w-[90%] ">
+                    <div className="relative bg-white border-3 border-central rounded-2xl pt-5 md:pt-12 pb-5 text-center shadow-sm mx-2 my-8 md:m-10 w-[85%] md:w-[80%] ">
                         <div className="absolute -top-5 left-1/2 transform -translate-x-1/2">
                             <div className="w-20 h-20 rounded-full bg-gradient-to-b from-primary to-midnight border-4 border-central flex items-center justify-center shadow-md">
                                 <Service_05 />
                             </div>
                         </div>
                         <div className="mt-10">
-                            <h3 className="font-fz-poppins font-bold text-lg uppercase text-primary  mb-2 text-pretty">GÓI<br /> XÉT NGHIỆM</h3>
-                            <p className="font-fz-poppins font-midium text-md text-primary">Xem thêm <span>»</span></p>
+                            <h3 className="font-fz-poppins font-bold text-md lg:text-xl uppercase text-primary  mb-2 text-pretty">GÓI<br /> XÉT NGHIỆM</h3>
+                            <p className="font-fz-poppins font-midium text-sm lg:text-md text-primary">Xem thêm <span>»</span></p>
                         </div>
                     </div>
                 </SwiperSlide>
             </Swiper>
-            <div className="swiper-button-prev-custom absolute left-0 top-1/2 -translate-y-1/2 z-10 bg-primary p-2 rounded-full shadow-md cursor-pointer hover:bg-central transition-colors">
-                <FiChevronLeft className="text-white text-sm" />
-            </div>
-            <div className="swiper-button-next-custom absolute right-0 top-1/2 -translate-y-1/2 z-10 bg-primary p-2 rounded-full shadow-md cursor-pointer hover:bg-central transition-colors">
-                <FiChevronRight className="text-white text-sm" />
+            <div className="flex">
+                <div className="swiper-button-prev-custom mx-2  left-0 top-1/2 -translate-y-1/2 z-10 bg-primary p-2 rounded-full shadow-md cursor-pointer hover:bg-central transition-colors">
+                    <FiChevronLeft className="text-white text-sm" />
+                </div>
+                <div className="swiper-button-next-custom mx-2  right-0 top-1/2 -translate-y-1/2 z-10 bg-primary p-2 rounded-full shadow-md cursor-pointer hover:bg-central transition-colors">
+                    <FiChevronRight className="text-white text-sm" />
+                </div>
             </div>
         </div >
     );
