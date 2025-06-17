15150b8e561db476c830808893463f5ee143ee7f
HoangTran <HoangTranHoangkhongmacao@gmail.com>
2025-06-17 09:03:03 +0700

    cm setting

--- src/app/admin/settings/page.js
+++ src/app/admin/settings/page.js
@@ -3,7 +3,7 @@ import SettingsForm from "@/component/pages/admin/setting/SettingsForm";
 export default function DashboardPage() {
     return (
         <div className="p-10">
-            <SettingsForm />
+            {/* <SettingsForm /> */}
         </div>
     );
 }
