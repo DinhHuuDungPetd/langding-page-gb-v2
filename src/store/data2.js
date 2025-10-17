60a99611afd931bb3a2b6e8669eaa12403cd0427
HoangTran <HoangTranHoangkhongmacao@gmail.com>
2025-10-17 10:42:01 +0700

    fix bug

---
+++ src/store/data2.js
@@ -0,0 +1,163 @@
+export const services = [
+    {
+        id: 1,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE",
+        status: "ACTIVE"
+    },
+    {
+        id: 2,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 3,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 4,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 5,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 6,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 7,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 8,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 9,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 10,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 11,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 12,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 13,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 14,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 15,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 16,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 17,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 18,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 19,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+    {
+        id: 20,
+        name: "Lorem ipsum",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        images: "/images/news/image.webp",
+        time: "2023-04-09",
+        status: "ACTIVE"
+    },
+]
